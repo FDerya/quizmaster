309a2f45a5d849701301014f22281509945f0443
Dennis Koelemeijer <dennis.koelemeijer@hva.nl>
2023-12-17 14:54:31 +0100

    csv bestand naar database werkt nu

--- src/main/java/controller/LauncherDennis.java
+++ src/main/java/controller/LauncherDennis.java
@@ -1,11 +1,19 @@
-package view;
+package controller;
 
 import database.mysql.DBAccess;
+import database.mysql.QuestionDAO;
 import database.mysql.UserDAO;
+import model.Course;
+import model.Question;
+import model.Quiz;
 import model.User;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -14,16 +22,30 @@ public class LauncherDennis {
     private static final String filepath = "src/main/java/database/Gebruikers.csv";
     private static final File userFile = new File(filepath);
 
+    private static final String MYSQL_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String PREFIX_CONNECTION_URL =
+            "jdbc:mysql://localhost:3306/";
+    private static final String CONNECTION_SETTINGS = "?useSSL=false" +
+            "&allowPublicKeyRetrieval=true" +
+            "&useJDBCCompliantTimezoneShift=true" +
+            "&useLegacyDatetimeCode=false" +
+            "&serverTimezone=UTC";
+
     public static void main(String[] args) {
-        DBAccess dBaccess = new DBAccess("Quizmaster", "userQuizmaster", "pwQuizmaster");
+        final String databaseName = "Quizmaster";
+        final String mainUser = "userQuizmaster";
+        final String mainUserPassword = "pwQuizmaster";
+        DBAccess dBaccess = new DBAccess(databaseName, mainUser, mainUserPassword);
         UserDAO userDAO = new UserDAO(dBaccess);
 
         List<String> test = FileReaderToArray();
-        List<User> userList = listUsers(test)
+        List<User> userList = listUsers(test);
 
+        dBaccess.openConnection();
         for (User user : userList) {
             userDAO.storeOne(user);
         }
+        dBaccess.closeConnection();
     }
 
     public static List<String> FileReaderToArray() {
