81bb56335d8262d9c55439a7587106e510d9d693
Bianca Duijvesteijn <bianca.duijvesteijn@hva.nl>
2023-12-22 10:47:22 +0100

    kleine aanpassing aan GroupDAO getOneById

--- src/main/java/database/mysql/GroupDAO.java
+++ src/main/java/database/mysql/GroupDAO.java
@@ -3,7 +3,6 @@ package database.mysql;
 
 import model.*;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -46,7 +45,7 @@ public class GroupDAO extends AbstractDAO implements GenericDAO<Group> {
             preparedStatement.setInt(1, id);
             try (ResultSet resultSet = executeSelectStatement()) {
                 if (resultSet.next()) {
-                    return getGroupFromResultSet(resultSet);
+                    group = getGroupFromResultSet(resultSet);
                 }
             }
         } catch (SQLException sqlError) {
@@ -58,17 +57,9 @@ public class GroupDAO extends AbstractDAO implements GenericDAO<Group> {
     // Methode om een nieuwe groep op te slaan
     @Override
     public void storeOne(Group group) {
-<<<<<<< HEAD
-        if (!isValidGroup(group)) {
-            System.out.println("Ongeldige gegevens. Opslaan geannuleerd.");
-            return;
-        }
-        String sql = "INSERT INTO `group` (idUser, naamGroup, amountStudent)" +
-                " VALUES (?, ?, ?, ?);";
-=======
         String sql = "INSERT INTO `group` (idUser, nameGroup, amountStudent)" +
                 " VALUES (?, ?, ?);";
->>>>>>> refs/remotes/origin/master
+
         try {
             setupPreparedStatementWithKey(sql);
             preparedStatement.setInt(1, group.getUserName().getIdUser());
@@ -129,53 +120,4 @@ public class GroupDAO extends AbstractDAO implements GenericDAO<Group> {
     }
 }
 
- /*   public void storeOne(Group group) {
-        //Controleert of alle gegevens zijn ingevuld
-        if (group.getGroupName() == null || group.getCourseName() == null || group.getAmountStudent() < 0 || group.getUserName() == null) {
-            System.out.println("Ongeldige gegevens. Opslaan geannuleerd.");
-            return;
-        }
-        String sql = "INSERT INTO `group` (groupName, courseName, amountStudent, userName)" +
-                " VALUES (?, ?, ?, ?);";
-        try {
-            setupPreparedStatementWithKey(sql);
-            preparedStatement.setString(1, group.getGroupName());
-            preparedStatement.setString(2, group.getCourseName().getNameCourse());
-            preparedStatement.setInt(3, group.getAmountStudent());
-            preparedStatement.setString(4, String.valueOf(group.getUserName().getIdUser()));
-            int primaryKey = executeInsertStatementWithKey();
-            group.setIdGroup(primaryKey);
 
-            executeManipulateStatement();
-        } catch (SQLException sqlFout) {
-            System.out.println(sqlFout.getMessage());
-        }
-    }*/
-
-  /*  private Group getGroupFromResultSet(ResultSet resultSet) throws SQLException {
-        Group group;
-
-        int idGroup = resultSet.getInt("idGroup");
-        int idTeacher = resultSet.getInt("idTeacher");
-        String nameCourse = resultSet.getString("nameCourse");
-        String nameGroup = resultSet.getString("nameGroup");
-        int amountStudent = resultSet.getInt("amountStudent");
-        int coordinatorUserId = resultSet.getInt("userId");
-        String coordinatorUsername = resultSet.getString("userName");
-
-        // Ophalen van de bijbehorende User
-        User coordinator = userDAO.getOneById(coordinatorUserId);
-        if (coordinator == null) {
-            // Als we de gebruiker niet kunnen vinden op basis van userId, proberen we het op basis van gebruikersnaam
-            coordinator = userDAO.getAll().stream()
-                    .filter(user -> user.getUsername().equals(coordinatorUsername))
-                    .findFirst()
-                    .orElse(null);
-        }
-        String difficulty = resultSet.getString("difficultyCourse");
-        Course course = new Course(coordinator, nameCourse, difficulty);
-
-        group = new Group(idGroup, idTeacher, course, nameGroup, amountStudent, coordinator);
-        return group;
-
-        }*/
