7773a8f59b467d285bf4c4708ec90e01b6d25ec7
FatmaDerya <fdtatar@gmail.com>
2024-01-01 20:31:03 +0100

    Created ManageQuestionsController Add een methode: getQuestionCountForQuiz

--- src/main/java/controller/ManageQuestionsController.java
+++ src/main/java/controller/ManageQuestionsController.java
@@ -1,39 +1,76 @@
 package controller;
 
+import database.mysql.QuestionDAO;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import model.Question;
+import view.questionTestMain;
+
+import java.util.List;
 
 public class ManageQuestionsController {
-    @FXML
-    private ListView<?> questionList;
 
+    private final QuestionDAO questionDAO;
     @FXML
-    void doCreateQuestion(ActionEvent event) {
+    ListView<Question> questionList;
+    @FXML
+    Button deleteQuestion;
+    @FXML
+    Label warningLabel;
+    private Question question;
+
+
+    public ManageQuestionsController() {
+        this.questionDAO = new QuestionDAO(questionTestMain.getDBaccess());
+
 
     }
 
-    @FXML
-    void doDeleteQuestion(ActionEvent event) {
+    public void setup() {
+        List<Question> allQuestions = questionDAO.getAll();
+        ObservableList<Question> questionObservableList =
+                FXCollections.observableArrayList(allQuestions);
+        questionList.setItems(questionObservableList);
+        questionList.getSelectionModel().selectFirst();
+    }
+
+    // terug naar menu
+    public void doMenu(ActionEvent event) {
+        questionTestMain.getSceneManager().showWelcomeScene();
+    }
+
+    public void doCreateQuestion() {
+        Question selectedQuestion = questionList.getSelectionModel().getSelectedItem();
+        if (selectedQuestion != null) {
+            questionTestMain.getSceneManager().showCreateUpdateQuestionScene(selectedQuestion);
+        } else {
+            warningLabel.setVisible(true);
+            warningLabel.setText("Je moet eerst een vraag kiezen");
+        }
+    }
+
+
+    public void doUpdateQuestion(ActionEvent event) {
+        Question question = questionList.getSelectionModel().getSelectedItem();
+        questionTestMain.getSceneManager().showCreateUpdateQuestionScene(question);
 
     }
 
-    @FXML
-    void doMenu(ActionEvent event) {
 
+    public void doDeleteQuestion(ActionEvent event) {
+        Question selectedQuestion = questionList.getSelectionModel().getSelectedItem();
+        if (selectedQuestion != null) {
+            questionList.getItems().remove(selectedQuestion);
+
+        } else {
+            warningLabel.setVisible(true);
+            warningLabel.setText("Je moet eerst een vraag kiezen");
+        }
     }
-
-    @FXML
-    void doUpdateQuestion(ActionEvent event) {
-
-    }
-    public void setup() {}
-
-    public void doMenu(){}
-
-    public void doCreateQuestion(){}
-
-    public void doUpdateQuestion(){}
-
-    public void doDeleteQuestion(){}
 }
+
