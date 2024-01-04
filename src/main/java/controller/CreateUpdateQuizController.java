8386f655fb5ff0bc8670e644e053fb2e1653121f
Tom van Beek <tom.van.beek4@hva.nl>
2024-01-04 10:19:44 +0100

    ManageQuiz afgerond

--- src/main/java/controller/CreateUpdateQuizController.java
+++ src/main/java/controller/CreateUpdateQuizController.java
@@ -1,18 +1,12 @@
 package controller;
 
-import database.mysql.CourseDAO;
+import javafx.event.ActionEvent;
+import model.*;
 import database.mysql.DBAccess;
 import database.mysql.QuizDAO;
-import database.mysql.UserDAO;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextField;
-import model.*;
+import javafx.scene.control.*;
 import view.Main;
-
-import java.util.List;
 
 public class CreateUpdateQuizController {
     @FXML
@@ -22,13 +16,15 @@ public class CreateUpdateQuizController {
     @FXML
     private TextField nameTextField;
     @FXML
+    private TextField courseTextField;
+    @FXML
+    TextField amountTextField;
+    @FXML
+    private Label titelLabel;
+    @FXML
+    private TextField levelTextField;
+    @FXML
     ListView<Course> courseList;
-
-    @FXML
-    private MenuButton taskMenuButton;
-    @FXML
-    private MenuButton taskMenuButton1;
-
 
     private final DBAccess dbAccess;
 
@@ -37,44 +33,18 @@ public class CreateUpdateQuizController {
     }
 
     public void setup(Quiz quizOne) {
-        QuizDAO quizDAO = new QuizDAO(dbAccess);
-        MenuItem amount1 = new MenuItem("1");
-        amount1.setOnAction(actionEvent -> quizOne.setAmountQuestions(1));
-        MenuItem amount2 = new MenuItem("2");
-        amount2.setOnAction(actionEvent -> quizOne.setAmountQuestions(2));
-        MenuItem amount3 = new MenuItem("3");
-        amount3.setOnAction(actionEvent -> quizOne.setAmountQuestions(3));
-        MenuItem amount4 = new MenuItem("4");
-        amount4.setOnAction(actionEvent -> quizOne.setAmountQuestions(4));
-        MenuItem amount5 = new MenuItem("5");
-        amount5.setOnAction(actionEvent -> quizOne.setAmountQuestions(5));
-        MenuItem amount6 = new MenuItem("6");
-        amount6.setOnAction(actionEvent -> quizOne.setAmountQuestions(6));
-        MenuItem amount7 = new MenuItem("7");
-        amount7.setOnAction(actionEvent -> quizOne.setAmountQuestions(7));
-
-        taskMenuButton.getItems().add(amount1);
-        taskMenuButton.getItems().add(amount2);
-        taskMenuButton.getItems().add(amount3);
-        taskMenuButton.getItems().add(amount4);
-        taskMenuButton.getItems().add(amount5);
-        taskMenuButton.getItems().add(amount6);
-        taskMenuButton.getItems().add(amount7);
-
-        MenuItem level1 = new MenuItem("Beginner");
-        amount1.setOnAction(actionEvent -> quizOne.setLevel("Beginner"));
-        MenuItem level2 = new MenuItem("Medium");
-        amount2.setOnAction(actionEvent -> quizOne.setLevel("Medium"));
-        MenuItem level3 = new MenuItem("Gevorderd");
-        amount3.setOnAction(actionEvent -> quizOne.setLevel("Gevorderd"));
-        taskMenuButton.getItems().add(level1);
-        taskMenuButton.getItems().add(level2);
-        taskMenuButton.getItems().add(level3);
-
-        CourseDAO courseDAO = new CourseDAO(dbAccess, new UserDAO(dbAccess));
-        List<Course> courses = courseDAO.getAll();
-        courseList.getItems().addAll(courses.toString());
+        titelLabel.setText("Wijzig Quiz");
+        courseTextField.setText(String.valueOf(quizOne.getCourse().getNameCourse()));
+        nameTextField.setText(quizOne.getNameQuiz());
+        levelTextField.setText(String.valueOf(quizOne.getLevel()));
+        amountTextField.setText(String.valueOf(quizOne.getAmountQuestions()));
+        amountTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                amountTextField.setText(oldValue);
+            }
+        });
     }
+    public void doMenuBack(ActionEvent event) { Main.getSceneManager().showManageQuizScene();}
 
     public void doMenu() {
         Main.getSceneManager().showWelcomeScene();
@@ -83,12 +53,12 @@ public class CreateUpdateQuizController {
     public void doCreateUpdateQuiz() {
         Quiz quiz = null;
         QuizDAO quizDAO = new QuizDAO(dbAccess);
-        CourseDAO courseDAO = new CourseDAO(dbAccess, new UserDAO(dbAccess));
         Course course = courseList.getSelectionModel().getSelectedItem();
-        String nameQuiz = nameTextField.getText();
-        String level = taskMenuButton1.getText();
-        int amountQuestions = quiz.getAmountQuestions();
+        String nameQuiz = quizList.getSelectionModel().getSelectedItem().getNameQuiz();
+        String level = nameTextField.getText();
+        int amountQuestions = 5;
         quiz = new Quiz(course, nameQuiz, level, amountQuestions);
         quizDAO.storeOne(quiz);
     }
+
 }
--- src/main/java/controller/ManageQuizzesController.java
+++ src/main/java/controller/ManageQuizzesController.java
@@ -4,20 +4,20 @@ import database.mysql.QuizDAO;
 import database.mysql.DBAccess;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
 import javafx.scene.control.TextField;
 import model.*;
 import view.Main;
-
 
 import java.util.List;
 
 public class ManageQuizzesController {
     @FXML
     ListView<Quiz> quizList;
+
+    private final DBAccess dbAccess;
     @FXML
     TextField waarschuwingsTextField;
-    private final DBAccess dbAccess;
 
 
     public ManageQuizzesController() {
@@ -40,20 +40,26 @@ public class ManageQuizzesController {
         Main.getSceneManager().showWelcomeScene();
     }
 
-    public void doCreateQuiz() {
-        Quiz quiz = new Quiz(null);
-         Main.getSceneManager().showCreateUpdateQuizScene(quiz);
 
+    public void doCreateQuiz(ActionEvent event) {
+        Quiz quiz = quizList.getSelectionModel().getSelectedItem();
+        if (quiz == (null)) {
+            waarschuwingsTextField.setVisible(true);
+            waarschuwingsTextField.setText("Je moet eerst een quiz kiezen");
+        } else {
+            Main.getSceneManager().showCreateUpdateQuizScene(quiz);
+
+        }
     }
 
-    public Quiz doUpdateQuiz(ActionEvent event) {
+    public void doUpdateQuiz(ActionEvent event) {
         Quiz quiz = quizList.getSelectionModel().getSelectedItem();
         if (quiz == null) {
             waarschuwingsTextField.setVisible(true);
             waarschuwingsTextField.setText("Je moet eerst een quiz kiezen");
         } else {
             Main.getSceneManager().showCreateUpdateQuizScene(quiz);
-        }return quiz;
+        }
     }
 
     public void doDeleteQuiz(ActionEvent event) {
