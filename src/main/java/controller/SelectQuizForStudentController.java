25c3c973dfcb7bb635eaa559ee2256abe3b64e1f
Tom van Beek <tom.van.beek4@hva.nl>
2024-01-26 16:00:38 +0100

    Quiz kiezen door student aangemaakt

--- src/main/java/controller/SelectQuizForStudentController.java
+++ src/main/java/controller/SelectQuizForStudentController.java
@@ -1,17 +1,45 @@
 package controller;
 
+import database.mysql.*;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
-import model.Quiz;
+import model.*;
+import view.Main;
 
-public class SelectQuizForStudentController {
+import java.util.List;
 
+public class SelectQuizForStudentController extends WarningAlertController{
+    private UserDAO userDAO;
+    private QuizDAO quizDAO;
+    private ParticipationDAO participationDAO;
     @FXML
     ListView<Quiz> quizList;
 
-    public void setup() {}
+    public SelectQuizForStudentController() {
+        this.quizDAO = new QuizDAO(Main.getDBaccess());
+        this.userDAO = new UserDAO(Main.getDBaccess());
+        this.participationDAO = new ParticipationDAO(Main.getDBaccess());
+    }
 
-    public void doMenu() {}
+    public void setup() {
+    User user = User.getCurrentUser();
+    Participation oneQuiz = participationDAO.getOneById(user.getIdUser());
+    int course = oneQuiz.getCourse().getIdCourse();
+    List<Quiz> quizzen = quizDAO.getAllByCourseId(course);
+    quizList.getItems().addAll(quizzen);
+}
 
-    public void doQuiz() {}
+    public void doMenu(ActionEvent event) {
+        Main.getSceneManager().showWelcomeScene();
+    }
+
+    public void doQuiz(ActionEvent event) {
+        Quiz quiz = quizList.getSelectionModel().getSelectedItem();
+        if(quiz == null){
+            setEmptyChoice("quiz",true);
+        }else{
+            Main.getSceneManager().showFillOutQuiz(quiz);
+        }
+    }
 }
