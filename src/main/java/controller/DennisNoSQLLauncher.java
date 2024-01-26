6b07c83f1e4266ac07a20225185c89053921057f
Dennis Koelemeijer <dennis.koelemeijer@hva.nl>
2024-01-26 12:27:22 +0100

    NoSQL launcher Dennis afgemaakt

--- src/main/java/controller/DennisNoSQLLauncher.java
+++ src/main/java/controller/DennisNoSQLLauncher.java
@@ -1,6 +1,5 @@
 package controller;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import javacouchdb.CouchDBAccess;
 import javacouchdb.UserCouchDBDAO;
@@ -10,11 +9,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class DennisNoSQLLauncher {
     public static void main(String[] args) {
-        Gson gson = new Gson();
         // Making testdata
         User testUserOne = new User(1, "Test", "test", "Frits", null, "Sissing", "Student");
         User testUserTwo = new User(2, "Dekoe", "dekoe", "Dennis", null, "Koelemeijer", "Functioneel Beheerder");
@@ -33,37 +32,43 @@ public class DennisNoSQLLauncher {
         // Open connection
         couchDBAccess.getClient();
 
-//        // Create user in couchDB
-//        for (User user : testUsers) {
-//            userCouchDBDAO.saveSingleUser(user);
-//        }
+        // Create user in couchDB
+        for (User user : testUsers) {
+            userCouchDBDAO.saveSingleUser(user);
+        }
 
-        // Write database to .csv file
+        // Write database to .csv file, sorted by idUser
         File userFile = new File("src/resources/usersFromCouchDB.csv");
         try {
+            List<JsonObject> allUsers = userCouchDBDAO.getAllDocuments();
             PrintWriter printWriter = new PrintWriter(userFile);
-            for (JsonObject user : userCouchDBDAO.getAllDocuments()) {
-                String userToJson = gson.toJson(user);
-                System.out.println(userToJson);}
+            allUsers.sort(Comparator.comparing(user -> user.get("idUser").getAsInt()));
+            for (JsonObject user : allUsers) {
+                user.remove("_id");
+                user.remove("_rev");
+                printWriter.println(user.getAsJsonObject());
+            }
             printWriter.close();
         } catch (FileNotFoundException fileNotFound) {
             System.out.println("Het bestand kan niet worden aangemaakt");
         }
 
 
-//        // Read user
-//        System.out.println("Student uit couchDB gehaald met idUser = 1:");
-//        System.out.println(userCouchDBDAO.getUser(1));
-//
-//        // Update user
-//        testUserOne.setRole("Docent");
-//        userCouchDBDAO.updateUser(testUserOne);
-//        System.out.println("The student becomes the master:");
-//        System.out.println(userCouchDBDAO.getUser(1));
-//
-//        // Delete user
-//        userCouchDBDAO.deleteUser(testUserOne);
-//        System.out.println("\nHelaas wordt Frits ontslagen en uit het systeem verwijderd");
-//        System.out.println("Hier stond ooit Frits Sissing, maar nu: " + userCouchDBDAO.getUser(1));
+        // Read user
+        System.out.println("Student uit couchDB gehaald met idUser = 1:");
+        User getUser = userCouchDBDAO.getUser(1);
+        System.out.println(getUser + ", " + getUser.getRole());
+
+        // Update user
+        testUserOne.setRole("Docent");
+        userCouchDBDAO.updateUser(testUserOne);
+        User updatedUser = userCouchDBDAO.getUser(1);
+        System.out.println("The student becomes the master:");
+        System.out.println(updatedUser + ", " + updatedUser.getRole());
+
+        // Delete user
+        userCouchDBDAO.deleteUser(testUserOne);
+        System.out.println("\nHelaas wordt Frits ontslagen en uit het systeem verwijderd");
+        System.out.println("Hier stond ooit Frits Sissing, maar nu: " + userCouchDBDAO.getUser(1));
     }
 }
