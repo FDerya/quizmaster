8550c4bf24a2e8671ee9aaa3ecbbe179687ee3c2
Dennis Koelemeijer <dennis.koelemeijer@hva.nl>
2024-01-29 13:19:17 +0100

    werk aan FIQ gedaan

--- src/main/java/controller/FillOutQuizController.java
+++ src/main/java/controller/FillOutQuizController.java
@@ -10,11 +10,12 @@ import model.Quiz;
 import view.Main;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class FillOutQuizController {
-    QuestionDAO questionDAO;
-
+    QuestionDAO questionDAO = new QuestionDAO(Main.getDBaccess());
+    String givenAnswer = "";
     @FXML
     private Label titleLabel;
     @FXML
@@ -25,28 +26,76 @@ public class FillOutQuizController {
     private Button nextQuestionButton;
 
     public void setup(Quiz quiz) {
-        questionDAO = new QuestionDAO(Main.getDBaccess());
+        List<Question> questionList = questionDAO.getQuestionsForQuiz(quiz);
+        Collections.shuffle(questionList);
         if (titleLabel.getText().equals("Vraag 1")) {
-         previousQuestionButton.setVisible(false);
-         questionArea.setText("test");
-         List<Question> questionList = questionDAO.getQuestionsForQuiz(quiz);
+            previousQuestionButton.setVisible(false);
         }
+        String question = questionList.get(0).getQuestion();
+        String correctAnswer = questionList.get(0).getAnswerRight();
+        String wrongAnswer1 = questionList.get(0).getAnswerWrong1();
+        String wrongAnswer2 = questionList.get(0).getAnswerWrong2();
+        String wrongAnswer3 = questionList.get(0).getAnswerWrong3();
+
+        List<String> answerList = new ArrayList<>();
+        answerList.add(correctAnswer);
+        answerList.add(wrongAnswer1);
+        answerList.add(wrongAnswer2);
+        answerList.add(wrongAnswer3);
+        Collections.shuffle(answerList);
+        questionArea.setText(titleLabel.getText() +
+                "\n" + question + "\n" +
+                "\nA: " + answerList.get(0) +
+                "\nB: " + answerList.get(1) +
+                "\nC: " + answerList.get(2) +
+                "\nD: " + answerList.get(3));
+
     }
 
-    public void doRegisterA() {}
+    public void doRegisterA() {
+        givenAnswer = "A";
+    }
 
-    public void doRegisterB() {}
+    public void doRegisterB() {
+        givenAnswer = "B";
+    }
 
-    public void doRegisterC() {}
+    public void doRegisterC() {
+        givenAnswer = "C";
+    }
 
-    public void doRegisterD() {}
+    public void doRegisterD() {
+        givenAnswer = "D";
+    }
 
-    public void doNextQuestion() {}
+    public void doNextQuestion() {
+        System.out.println(givenAnswer);
+    }
 
     public void doPreviousQuestion() {}
 
     @FXML
     private void doMenu() {
         Main.getSceneManager().showWelcomeScene();
+    }
+
+    private List<Question> shuffleQuestions(List<Question> oldList) {
+        List<Question> newList = new ArrayList<>();
+        for (int i = 1; i <= oldList.size(); i++) {
+            int randomNumber = (int) (Math.random() * oldList.size());
+            newList.add(oldList.get(randomNumber));
+            oldList.remove(randomNumber);
+        }
+        return newList;
+    }
+
+    private List<String> shuffleAnswers(List<String> oldList) {
+        List<String> newList = new ArrayList<>();
+        for (int i = 1; i <= oldList.size(); i++) {
+            int randomNumber = (int) (Math.random() * oldList.size());
+            newList.add(oldList.get(randomNumber));
+            oldList.remove(randomNumber);
+        }
+        return newList;
     }
 }
